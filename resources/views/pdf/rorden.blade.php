c08b543d289a56c401304c2e5d9a6af8f8f13c4c
fjmontejo <montejohfrancisco@gmail.com>
2018-07-20 23:18:54 -0500

    agregué nuevo formato pdf

---
+++ resources/views/pdf/rorden.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+  <head>
+    <meta charset="utf-8">
+    <title>ORDEN DE SERVICIO</title>
+  </head>
+  <body>
+      <div>
+          <img src="escudo.jpg" alt="Smiley face" width="200" height=150 style="float:left">
+          <img src="sec.png" alt="Smiley face" width="300" height=150 style="float:right">
+        </div>
+    
+    <center><h1>SECRETAR&IacuteA DE SALUD </h1>
+    <h2> HOSPITAL REGIONAL</h2>
+    <h2>" DR RAFAEL PASCACIO GAMBOA"</h2>
+  </center>
+<hr>
+<hr>
+<center><p> 
+  
+    <font size="5" color="#2F4F4F"> ORDEN DE SERVICIO </font>
+   </p></center>
+   <HR>
+<BR>
+   <right><p> 
+     FECHA   <input type="text" size="35" maxlength="80"  name="FECHA">
+     <BR>
+      <BR><BR>
+
+     &AacuteREA   <input type="text" size="35" maxlength="80"  name="AREA">
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     PERSONAL QUE REPORTA   <input type="text" size="35" maxlength="80"  name="PERSONAL">
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     TURNO   <input type="text" size="35" maxlength="80"  name="TURNO">
+     
+   </p>
+   <BR>
+   <CENTER><h3>TIPO DE SERVICIO</h3></CENTER>
+   <HR>
+   <BR>
+<table width="100%" border="1" cellspacing="5" cellpadding="10">
+  <tr>
+      <td>
+          MP <input type="checkbox" name="MP" size="50">
+
+      </td>
+      
+  
+      <td>
+           MC <input type="checkbox" name="MC" size="50">
+      </td>
+      <td>
+          INSTALACI&OacuteN <input type="checkbox" name="INSTALACION" size="50">
+      </td>
+      <td>
+          ASESORIA <input type="checkbox" name="ASESORIA" size="50">
+      </td>
+      <td>
+          RETIRO <input type="checkbox" name="RETIRO" size="50">
+      </td>
+          
+    </tr> 
+  
+  
+</table>
+<CENTER><h3>DATOS DEL EQUIPO</h3></CENTER>
+<HR>
+<BR>
+  <div>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      EQUIPO   <input type="text" size="35" maxlength="80"  name="EQUIPO">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      MARCA   <input type="text" size="35" maxlength="80"  name="MARCA">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      MODELO   <input type="text" size="35" maxlength="80"  name="MODELO">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <BR><BR>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       ACTIVO FIJO   <input type="text" size="35" maxlength="80"  name="ACTIVO">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       Nยบ SERIE   <input type="text" size="35" maxlength="80"  name="NUMSERIE">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      
+  
+  </div>
+        
+  <BR><BR>
+    <HR>
+<BR>
+  PROBLEMA REPORTADO / RAZ&OacuteN DEL SERVICIO
+  <textarea name="PREPORTADO" rows="6" cols="185"></textarea>    
+  PROBLEMA ENCONTRADO / CONDICI&OacuteN ENCONTRADA
+  <textarea name="PREPORTADO" rows="6" cols="185"></textarea>    
+  ACCIONES TOMADAS
+  <textarea name="PREPORTADO" rows="6" cols="185"></textarea>    
+REFACCIONES UTILIZADAS  
+<textarea name="PREPORTADO" rows="6" cols="185"></textarea>  
+OBSERVACIONES
+<textarea name="PREPORTADO" rows="6" cols="185"></textarea>  
+<br><br><br>
+<BR>
+  <br><BR><BR>
+    <hr>
+    <br>
+    <br>
+    <p> <input type="text" size="45" maxlength="80"  name="INGSERVICIO">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        
+       <input type="text" size="45" maxlength="80"  name="JEFE"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+         <input type="text" size="45" maxlength="80"  name="ACEPTACION">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+    
+<p>INGENIERO DE SERVICIO(NOMBRE Y FIRMA) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  JEFE DE BIOMEDICA(NOMBRE Y FIRMA)
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  ACEPTACI&OacuteN DE LA ORRDEN DE SERVICIO </p>
+
+  </body>
+</html>
